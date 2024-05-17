a748d0dd816f65b6ad016a789271f2198d7c0849
Naeem Shaikh <naeem.shaikh@contentstack.com>
2024-05-17 15:45:06 +0530

    set x-forwarded for

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -3,8 +3,11 @@ export default async function handler(req, context) {
   const route = parsedUrl.pathname;
   const envVariable = context.env.TEST_KEY;
   console.log(req.headers.get('CF-Connecting-IP'))
-
+  const modifiedReq = new Request(request)
+  modifiedReq.headers.set("x-forwarded-for", modifiedReq.headers.get('CF-Connecting-IP'))
+  console.log(modifiedReq.headers.get('x-forwarded-for'))
   if (route === '/test') {
+
     const modifiedRequest = new Request(new URL('/users', parsedUrl), req)
     const requestWithCF = new Request(modifiedRequest)
     const res = await fetch(requestWithCF);
