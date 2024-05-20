1b1b826ccab83f41dc6937e06185ff01f0c10839
Naeem Shaikh <naeem.shaikh@contentstack.com>
2024-05-20 10:27:42 +0530

    log chache exists

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -19,7 +19,11 @@ export default async function handler(req, context) {
   if (route === '/external') {
     let cache = caches.default;
     const modifiedRequest = new Request('https://4d61-103-239-86-172.ngrok-free.app', req)
-    cache.delete(modifiedRequest, {ignoreMethod: true});
+    const cacheOptions = {ignoreMethod: true}
+    const cacheExists = await cache.match(modifiedRequest, cacheOptions);
+    console.log('exists='+cacheExists)
+    const deleted = await cache.delete(modifiedRequest, cacheOptions);
+    console.log('deleted='+deleted)
 
     // const modifiedRequest = new Request('https://webhook.site/2f864b70-aabe-4d17-9964-29ed6d59c719?id=1', req)
     // const modifiedRequest = new Request('https://dummytest.requestcatcher.com/test', req)
