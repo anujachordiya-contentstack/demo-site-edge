82cdc024a45b7b8ca57987c66b1d1dcfdb65f857
Naeem Shaikh <naeem.shaikh@contentstack.com>
2024-05-20 10:03:34 +0530

    cache ttl 0

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -23,7 +23,7 @@ export default async function handler(req, context) {
     // const modifiedRequest = new Request('https://dummytest.requestcatcher.com/test', req)
     const requestWithCF = new Request(modifiedRequest)
     return fetch(requestWithCF, {
-      cf: {cacheTtl: -1}
+      cf: {cacheTtl: 0}
     });
   }
 
