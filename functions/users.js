27c0fce894a1dfc539a1bd92f50803db55e3d9ae
naeem-shakh-cs <165668796+naeem-shakh-cs@users.noreply.github.com>
2024-08-13 20:42:55 +0530

    Update users.js

--- functions/users.js
+++ functions/users.js
@@ -8,7 +8,6 @@ export default function handler(request, response) {
 
   console.log("query params", request.query)
   console.log("env", process.env.test_env)
-  console.log("env", process.env.test_env_2)
 
   response.status(200).send({
     body: request.body,
