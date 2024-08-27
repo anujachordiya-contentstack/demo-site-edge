fd0b85af803b72f9f2e6ca6b0afa4d66ab9849cd
naeem-shakh-cs <165668796+naeem-shakh-cs@users.noreply.github.com>
2024-08-27 22:19:15 +0530

    Update [proxy].edge.js

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -2,7 +2,6 @@ export default async function handler(req, context) {
   const parsedUrl = new URL(req.url);
   const route = parsedUrl.pathname;
   const envVariable = context.env.TEST_KEY;
-  console.log('vars length:', Object.keys(context.env));
   if (route === '/test') {
     const res = await fetch(`https://random-data-api.com/api/v2/appliances`);
     let response = await res.json();
