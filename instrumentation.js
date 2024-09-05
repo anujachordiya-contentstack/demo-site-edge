9e3485f9ff8677b80acf732bf8895a23b2fcc5e7
Naeem Shaikh <naeem.shaikh@contentstack.com>
2024-09-05 17:06:51 +0530

    log

--- instrumentation.js
+++ instrumentation.js
@@ -1,5 +1,9 @@
 export async function register() {
+  console.log('A')
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    console.log('B')
     await import('./instrumentation.node.ts')
+    console.log('C')
   }
+  console.log('D')
 }
