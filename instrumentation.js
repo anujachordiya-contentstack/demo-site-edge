9a6ebd7005b35a7696141dbd6993559911a8ce2b
Naeem Shaikh <naeem.shaikh@contentstack.com>
2024-09-05 17:29:16 +0530

    setup

--- instrumentation.js
+++ instrumentation.js
@@ -2,7 +2,7 @@ export async function register() {
   console.log('A')
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     console.log('B')
-    await import('./instrumentation.node.ts')
+    await import('./instrumentation.node.js')
     console.log('C')
   }
   console.log('D')
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import { useEffect } from 'react';
+import { register } from '../instrumentation.js';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Call the register function
+    register();
+  }, []);
   return <Component {...pageProps} />;
 }
