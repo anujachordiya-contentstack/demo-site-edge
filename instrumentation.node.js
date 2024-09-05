7a306be5bb0447a4a9b1f88a9bf336f3dacf90a1
Naeem Shaikh <naeem.shaikh@contentstack.com>
2024-09-05 15:21:57 +0530

    add token

--- instrumentation.node.js
+++ instrumentation.node.js
@@ -5,9 +5,12 @@ import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions'
 import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node'
 
 const otelUrl = process.env.OTEL_URL; // 'https://your-otel-url.com'
+const otelAuthToken = process.env.OTEL_AUTH;
 
 const traceExporter = new OTLPTraceExporter({
-  url: otelUrl,
+  url: otelUrl, headers: {
+    'Authorization': `Bearer ${otelAuthToken}`,
+  },
 })
 
 const sdk = new NodeSDK({
