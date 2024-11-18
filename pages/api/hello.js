e74af3c298f21d07f9b17a87d1d10f8f3d7cba0d
naeem-shakh-cs <165668796+naeem-shakh-cs@users.noreply.github.com>
2024-11-18 10:46:30 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  console.log('method', req.method)
   console.log('vars length:',process.env.AH_COLLECTION_ID5);
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json({ name: "John Doe" });
