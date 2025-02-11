63eda46e453594cd7c5aac914d73df92ca6f9a4c
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-02-11 14:46:35 +0530

    remove all code except cloud func

--- pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  console.log('method', req.method)
-  console.log('vars length:',process.env.AH_COLLECTION_ID5);
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json({ name: "John Doe" });
-}
