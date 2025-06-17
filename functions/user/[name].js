6846c918db3453963787f88474baf98368e678b3
naeem-shakh-cs <165668796+naeem-shakh-cs@users.noreply.github.com>
2025-06-17 12:56:44 +0530

    Update [name].js

--- functions/user/[name].js
+++ functions/user/[name].js
@@ -1,4 +1,4 @@
 export default async function handler(request, response) {
   await new Promise(resolve => setTimeout(resolve, 5000)); // wait for 5 seconds
-  response.status(200).send(`Hello ${request.query.name}`);
+  response.status(200).send(`Hello ${request.params.name}`);
 }
