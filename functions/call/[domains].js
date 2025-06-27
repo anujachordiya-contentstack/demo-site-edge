d5f2d30fca8d0aa6bd4d495698fff92c9bd2cb05
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-06-27 20:55:50 +0530

    catch

--- functions/call/[domains].js
+++ functions/call/[domains].js
@@ -1,5 +1,11 @@
 export default async function handler(request, response){
+  try{
+
   const res = await fetch(process.env.API_URL + `/domain/${request.params.domains}`)
   const data = await res.json()
   response.status(200).send(data);
+  }catch(error){
+    console.error(error);
+    response.status(500).send({ error: 'Internal Server Error custom' });
+  }
 }
