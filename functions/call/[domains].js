406b458ddf7c9f1b874ef0f32231d6268b81dae5
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-06-27 20:47:07 +0530

    fix

--- functions/call/[domains].js
+++ functions/call/[domains].js
@@ -1,5 +1,5 @@
 export default async function handler(request, response){
-  const res = await fetch(ProcessingInstruction.env.API_URL + `/domains/${request.params.domains}`)
+  const res = await fetch(process.env.API_URL + `/domains/${request.params.domains}`)
   const data = await res.json()
   response.status(200).send(data);
 }
