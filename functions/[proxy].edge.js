07d82d11297058aeb15ec82eb54ef372b12e39e9
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-09-11 12:42:39 +0530

    add test route

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -3,6 +3,22 @@ export default async function handler(req, context) {
   const route = parsedUrl.pathname;
   const envVariable = context.env.TEST_KEY;
   
+  if (route === '/test') {
+    const res = await fetch(`https://random-data-api.com/api/v2/appliances`);
+    let response = await res.json();
+    response = {
+      ...response,
+      time: new Date(),
+      envVariableValue: envVariable,
+    }
+    return new Response(JSON.stringify(response), {
+      headers: {
+        'X-Message': 'Change response headers',
+        'server': 'Launch'
+      }
+    })
+  }
+  
   const userAgent = req.headers.get('user-agent');
 console.log("requested", route)
   const redirectRequest = new Request(`https://contentstack-com-implement-redirects.devcontentstackapps.com${route}`);
