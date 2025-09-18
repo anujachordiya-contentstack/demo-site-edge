0030a417f3d6268e18aff41f0b7bade011b1465e
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-09-18 12:13:04 +0530

    add host header

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -4,7 +4,7 @@ export default async function handler(req, context) {
   const envVariable = context.env.TEST_KEY;
   
   if (route === '/test') {
-    const res = await fetch(`https://contentstack-com-implement-redirects.contentstackapps.com`);
+    const res = await fetch(`https://contentstack-com-implement-redirects.contentstackapps.com/123`,{headers:{ "Host":"contentstack-com-implement-redirects.contentstackapps.com"}});
     return res;
   }
 
