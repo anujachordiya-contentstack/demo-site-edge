e932c97b7492778e4ff67379fb1791bb29dc7213
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-09-18 11:48:07 +0530

    rewrite

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -4,7 +4,7 @@ export default async function handler(req, context) {
   const envVariable = context.env.TEST_KEY;
   
   if (route === '/test') {
-    const res = await fetch(`https://dummyjson.com/users/1`);
+    const res = await fetch(`contentstack-com-implement-redirects.contentstackapps.com`);
     let response = await res.json();
     response = {
       ...response,
@@ -18,29 +18,6 @@ export default async function handler(req, context) {
       }
     })
   }
-  
-
-  const userAgent = req.headers.get('user-agent');
-console.log("requested", route)
-  const redirectRequest = new Request(`https://contentstack-com-implement-redirects.devcontentstackapps.com${route}`);
-    const res = await fetch(redirectRequest);
-    console.log(res.status)
-    // console.log(res.headers.getAll())
-    if(!res.ok){
-      return new Response('Internal Server Error', { status: 500 });
-    }
-    let response = await res.json();
-    response = {
-      ...response,
-      time: new Date(),
-      envVariableValue: envVariable,
-    }
-    return new Response(JSON.stringify(response), {
-      headers: {
-        'X-Message': 'Change response headers',
-        'server': 'Launch'
-      }
-    })
 
   return fetch(req)
 }
