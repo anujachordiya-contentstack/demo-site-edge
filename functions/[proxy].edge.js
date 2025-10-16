64b7762e0d5acde94c7ac4f720fea936b819cfa1
naeem-shakh-cs <165668796+naeem-shakh-cs@users.noreply.github.com>
2025-10-16 12:08:06 +0530

    Update [proxy].edge.js

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -2,10 +2,10 @@ export default async function handler(req, context) {
   const parsedUrl = new URL(req.url);
   const route = parsedUrl.pathname;
   const envVariable = context.env.TEST_KEY;
-  console.log(req.headers.get('x-forwarded-for'));
+  // console.log(req.headers.get('x-forwarded-for'));
   
   const userAgent = req.headers.get('user-agent');
-  console.log('User-Agent:', userAgent);
+  // console.log('User-Agent:', userAgent);
   if (route === '/test') {
     const res = await fetch(`https://random-data-api.com/api/v2/appliances`);
     let response = await res.json();
