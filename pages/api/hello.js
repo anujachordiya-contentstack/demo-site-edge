00fb818ceeb903f7c2f7e629306e96fbc7166738
anujachordiya-contentstack <anuja.chordiya@contentstack.com>
2025-10-28 10:55:11 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -16,7 +16,7 @@ const logPayload = {
 
 
   // Log a full structured JSON payload — this is what might get filtered on Launch
-  console.log('localhost');
+  console.log('localhost:4');
   console.log('---- BEGIN STRUCTURED LOG ----');
   // console.log(logPayload);
 
